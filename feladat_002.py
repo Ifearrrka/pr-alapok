b2d019f4a42212b60def465618bb06c859de2074
morvaim <87572906+morvaimarcell@users.noreply.github.com>
2023-09-15 13:23:04 +0200

    Add files via upload

---
+++ feladat_002.py
@@ -0,0 +1,18 @@
+# feladat_002
+"""
+Kérjünk be a billentyűzetről két egész számot és írassuk ki a két szám összegét a képernyőre!
+"""
+szam_1 = input("Kérek egy számot: ")
+szam_1 = int(szam_1)
+szam_3 = float(szam_1)
+szam_2 = int(input("Kérek egy másik számot: "))
+
+#osszeg = szam_1 + szam_2
+print(f"A megadott két szám összege: {szam_1 + szam_2}")
+#print(f"A megadott két szám összege: {osszeg}")
+print(f"A megadott két szám összege: {type(szam_1)}")
+print(f"A megadott két szám összege: {type(szam_1)}")
+print(f"A megadott két szám összege: {type(szam_2)}")
+#print(f"A megadott két szám összege: {type(osszeg)}")
+#print(f"A megadott két szám összege: {szam_3}")
+#print(f"A megadott két szám összege: {type(szam_3)}")
